4a107f0c4f220d89f2d98204421bf68d25678274
Yoshiki Zukeyama <chocolaz1402@gmail.com>
2019-05-23 18:50:37 +0900

    ch08 Initial

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+
+    if user && user.authenticate(params[:session][:password])
+      # Success
+    else
+      # Failure
+      # flash[:denger]はTwitterのBootstrapで文字が赤いフラッシュができる
+      flash[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+
+  end
+end
