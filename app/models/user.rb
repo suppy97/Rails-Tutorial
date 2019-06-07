0525006462c73b2f6b88ed464cb15c83bc1f1007
Yoshiki Zukeyama <chocolaz1402@gmail.com>
2019-06-07 15:39:03 +0900

    ch10 initial

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ before_save { self.email = email.downcase }
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   validates :password, presence: true, length: { minimum: 6 }
 
   # 渡された文字列のハッシュ値を返す
